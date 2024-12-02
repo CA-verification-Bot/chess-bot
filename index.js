6e15ac8a793508df1f5596005ebfb0920a22cab5
Pratham Shah <itspratham2911@gmail.com>
2024-12-02 13:06:30 +0530

    refactor: :recycle: removed unwanted code

--- index.js
+++ index.js
@@ -4,26 +4,9 @@ import dotenv from 'dotenv'
 dotenv.config();
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 
-client.on('messageCreate', message => {
-    const lowerCaseMessage = message.content.toLowerCase();
-    if(lowerCaseMessage.includes('good night') && message.author.bot === false) {
-        message.react('🌜')
-        message.reply('Good Night borski!');
-    }
-})
-
 client.on('interactionCreate', async (interaction)=>{
     if(!interaction.isCommand()) return;
     
-    if(interaction.commandName === 'ping') {
-        try {
-            console.log('Sending gifs!');
-            await interaction.reply('Pong you!!')
-            await interaction.channel.send('https://tenor.com/view/pong-gif-26462133')
-        } catch (error) {
-            console.error('Error sending GIF:', error)
-        }
-    }
     if (interaction.commandName === "verify") {
         await interaction.deferReply();
     
@@ -73,7 +56,6 @@ client.on('interactionCreate', async (interaction)=>{
             );
         }
     }
-    
 })
 
 client.login(`${process.env.BOT_TOKEN}`)
